c7b84373954d5e6ce14070db1f5fed3ed954b6b4
libin40 <libin40@baidu.com>
2019-08-23 17:25:39 +0800

    a

--- service-worker.js
+++ service-worker.js
@@ -8,14 +8,13 @@ var filesToCache = [
     './assets/css/hello-world.css',
     './assets/icons/android-icon-48x48.png',
     './assets/icons/apple-icon-152x152.png',
-    './assets/icons/apple-icon-180x180.png',
-    'https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+    './assets/icons/apple-icon-180x180.png'
 ];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     console.log('[ServiceWorker] Install');
     e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
+        caches.open(cacheName).then(function (cache) {
             console.log('[ServiceWorker] Caching app shell');
 
             return cache.addAll(filesToCache);
@@ -23,11 +22,11 @@ self.addEventListener('install', function(e) {
     );
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
     console.log('[ServiceWorker] Activate');
     e.waitUntil(
-        caches.keys().then(function(keyList) {
-            return Promise.all(keyList.map(function(key) {
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
                 if (key !== cacheName && key !== dataCacheName) {
                     console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
@@ -39,29 +38,29 @@ self.addEventListener('activate', function(e) {
     return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
     console.log('[Service Worker] Fetch', e.request.url);
     var dataUrl = '/api';
     if (e.request.url.indexOf(dataUrl) > -1) {
         //https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
         e.respondWith(
             caches.open(dataCacheName)
-            .then(function(cache) {
-                return fetch(e.request).then(function(response) {
-                    cache.put(e.request.url, response.clone());
-                    return response;
-                });
-            })
+                .then(function (cache) {
+                    return fetch(e.request).then(function (response) {
+                        cache.put(e.request.url, response.clone());
+                        return response;
+                    });
+                })
         );
     } else {
         e.respondWith(
-            caches.match(e.request).then(function(response) {
+            caches.match(e.request).then(function (response) {
                 return response || fetch(e.request);
             })
         );
     }
 });
 
-self.addEventListener('push', function(e) {
+self.addEventListener('push', function (e) {
     console.log('[Service Worker] push: ', e);
 });
